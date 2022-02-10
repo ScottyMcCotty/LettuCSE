6a60f98ace6f7b22edb8674a468604feaccd3dc9
MiraWelner <miraewelner@gmail.com>
2022-02-10 14:54:21 -0800

    fixed variables for complaining martin

--- Client_Side/main.py
+++ Client_Side/main.py
@@ -1,23 +1,21 @@
-import sys
 from tray import Tray
 from arduino import Arduino
 from arduino_error import ArduinoError
 
-
 def end(arduino):
     '''Returns arm to the origin, exits without error'''
     arduino.move_toolhead((0,0))
-    print("Transplanting Completed")
-    sys.exit(0)
+    print("Repotting Completed")
+    exit(0)
 
 def shut_down():
     '''Quits program without returning main arm to the origin, program returns an error'''
     print("EMERGENCY SHUTDOWN")
-    sys.exit(1)
+    exit(1)
 
 def startup(arduino):
     '''Greets user and gives instructions for use'''
-    print("You have successfully initialized the LettuceCSE Lettuce Repotter, designed and implemented by Martin Orosa, Scott Ballinger, Mira Welner, and Liam Carr under the supervision of Dr. Lith\n"
+    print("You have successfully initialized the LettuceCSE Lettuce Repotter, designed and implemented by Marin Orosa, Scott Ballinger, Mira Welner, and Liam Carr under the supervision of Professor Lieth\n"
             "Use a keyboard interrupt (control c) to instantly freeze arm and shut down program\n"
             "Press 'e' when prompted to move arm to origin and end program\n"
             "Press any other key to begin")
@@ -63,37 +61,6 @@ def test_trays(tray_1, tray_2, arduino):
                         "Press e to end and any other key to continue")
         ask_to_quit(arduino)
 
-def transplant(source_tray, destination_tray, arduino):
-    '''
-    transplants the lettuce plants, notify user when a tray must be replaced
-
-            Parameters:
-                    source_tray (Tray): the tray from which the lettuce is being moved
-                    tray_2 (Tray): the tray to which the lettuce is being moved
-                    arduino (Arduino): The arduino object being used for the arm
-    '''
-    source_hole = destination_hole = 0
-    try:
-        while(True):
-            if source_hole == source_tray.get_number_of_holes():
-                print("Tray is empty - Press 'e' to end repotting or any other key to continue after tray is replaced\n")
-                ask_to_quit(arduino)
-                source_hole = 0
-            elif destination_hole == destination_tray.get_number_of_holes():
-                print("Tray is full - Press 'e' to end repotting or any other key to continue after tray is replaced\n")
-                ask_to_quit(arduino)
-                destination_hole = 0
-            else:
-                try:
-                    repot_single_plant(source_tray.ith_hole_location(source_hole), destination_tray.ith_hole_location(destination_hole), arduino)
-                except(ArduinoError):
-                    shut_down()
-                source_hole+=1
-                destination_hole+=1
-
-    except KeyboardInterrupt:
-        shut_down()
-
 
 def main():
 
@@ -102,12 +69,33 @@ def main():
 
     arduino_in_use = Arduino(0.14)
 
+    source_hole = destination_hole = 0
+
     test_trays(source_tray, destination_tray, arduino_in_use)
 
     startup(arduino_in_use)
     
 
-    transplant(source_tray, destination_tray, arduino_in_use)
+    try:
+        while(True):
+            if source_hole == source_tray.get_number_of_holes():
+                print("Tray is empty - Press 'e' to end repotting or any other key to continue after tray is replaced\n")
+                ask_to_quit(arduino_in_use)
+                source_hole = 0
+            elif destination_hole == destination_tray.get_number_of_holes():
+                print("Tray is full - Press 'e' to end repotting or any other key to continue after tray is replaced\n")
+                ask_to_quit(arduino_in_use)
+                destination_hole = 0
+            else:
+                try:
+                    repot_single_plant(source_tray.ith_hole_location(source_hole), destination_tray.ith_hole_location(destination_hole), arduino_in_use)
+                except(ArduinoError):
+                    shut_down()
+                source_hole+=1
+                destination_hole+=1
+
+    except KeyboardInterrupt:
+        shut_down()
 
 if __name__ == "__main__":
     main()
