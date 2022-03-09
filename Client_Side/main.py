2a7177d7c0f6d1896c6ff240c8e87c8a42b01eb7
Mira Welner <miraewelner@gmail.com>
2022-03-08 19:33:01 -0800

    start limit switch

--- Client_Side/main.py
+++ Client_Side/main.py
@@ -1,12 +1,23 @@
 """When run, this module makes the robot begin transplanting"""
+import yaml
 from transplanter_robot import TransplanterRobot
 from gui import GUI
 from tray import Tray
 from frame_arduino import FrameArduino
 from toolhead_arduino import ToolheadArduino
 
+
+def load_config_file():
+    """Load the config file which is stored in yaml"""
+    with open("config.yaml", "r") as config_file:
+        return yaml.full_load(config_file)
+
+
+
 def main():
     """Run the transplanter"""
+    config_file = load_config_file
+    print(config_file)
     source_tray = Tray('dense_tray.json')
     destination_tray = Tray('sparse_tray.json', source_tray.get_width())
     frame_arduino = FrameArduino()
