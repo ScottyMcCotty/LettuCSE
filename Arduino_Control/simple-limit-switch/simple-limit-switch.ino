e81548beacd6cefb1f899dbe00e2c16470e92938
ScottyMcCotty <sballinger@ucdavis.edu>
2022-03-08 19:53:15 -0800

    Added limit switch calibration

---
+++ Arduino_Control/simple-limit-switch/simple-limit-switch.ino
@@ -0,0 +1,18 @@
+// https://forum.arduino.cc/t/using-end-stops-on-cnc-shield-design-for-gbrl/510231/2
+int x_limit_switch_pin = 9;
+int count = 0;
+
+void setup() {
+  pinMode(x_limit_switch_pin, INPUT_PULLUP);
+  Serial.begin(9600);
+  delay(500);
+}
+
+void loop() {
+  int val = digitalRead(x_limit_switch_pin);
+  if (val == LOW) {
+    count++;
+    Serial.print("Press me harder, daddy x"); Serial.println(count);
+    delay(100);
+  }
+}
